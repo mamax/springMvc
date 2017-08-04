3564995f0ad3f718a3f8a305af256a624e26edf2
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-08-04 23:08:51 +0300

    Added_property_injection

--- src/main/java/com/epam/spring/mvc/beans/Client.java
+++ src/main/java/com/epam/spring/mvc/beans/Client.java
@@ -5,6 +5,12 @@ public class Client {
     private String id;
     private String fullName;
 
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    private String greeting;
+
     public Client(String id, String fullName) {
         this.id = id;
         this.fullName = fullName;
